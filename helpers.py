fc1f4069c2be4cba965e4af8ec94e54c5ab59e20
Sai Vamshi Ksheersagar <ksaivamsshi777@gmail.com>
2025-07-11 14:05:46 -0400

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -20,13 +20,10 @@ def get_recommendations(summary):
         tips.append("You're spending more than you earn. Consider reducing discretionary expenses.")
     else:
         tips.append("Good job saving this month! Consider investing or emergency funds.")
-
     if summary["Top Expense Category"].lower() in ["food", "entertainment"]:
-        tips.append(f"Spending a lot on {summary['Top Expense Category']}. Consider setting a budget limit.")
-
+        tips.append(f"High spend on {summary['Top Expense Category']}. Set a limit.")
     if summary["Savings Rate (%)"] > 20:
-        tips.append("Excellent savings rate! Keep it above 20% if possible.")
-
+        tips.append("Excellent savings rate! Keep it up.")
     return tips
 
 def calculate_savings_rate(income, savings):
